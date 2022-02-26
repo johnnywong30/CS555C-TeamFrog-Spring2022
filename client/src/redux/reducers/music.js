70c2f5f62817f703674651b6d66b6c77deef7e20
Jordan Wang <jordanwng58@gmail.com>
2022-02-25 23:39:38 -0500

    getSong helper

--- client/src/redux/reducers/music.js
+++ client/src/redux/reducers/music.js
@@ -1,3 +1,14 @@
+const fs = require('graceful-fs')
+const musicList = fs.readdirSync('../../constants')
+
+const getSong = (currentSong) => {
+    let newSong = musicList[Math.floor(Math.random() * musicList.length)]
+    while (currentSong === newSong) {
+        newSong = musicList[Math.floor(Math.random() * musicList.length)]
+    }
+    return newSong
+}
+
 const INIT_STATE = {
     playing: false,
     source: '',
@@ -9,19 +20,23 @@ const musicReducer = (state = INIT_STATE, action) => {
     switch (type) {
         case "PLAY_MUSIC":
             return {
-                
+                ...state,
+                source: getSong(state.source)
             }
         case "PAUSE_MUSIC":
             return {
-
+                ...state,
+                playing: false
             }
         case "INCREASE_MUSIC":
             return {
-
+                ...state,
+                volume: (state.volume + 0.1)
             }
         case "DECREASE_MUSIC":
             return {
-
+                ...state,
+                volume: (state.volume - 0.1)
             }
         case "CHANGE_MUSIC":
             return {
