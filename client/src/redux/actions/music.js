aee7ba66e5801ec6c28be529de4c29a444d733bf
Jordan Wang <jordanwng58@gmail.com>
2022-02-25 23:15:54 -0500

    add music.js actions

---
+++ client/src/redux/actions/music.js
@@ -0,0 +1,39 @@
+export const startMusic = () => {
+    return async dispatch => {
+        dispatch({
+            type: "PLAY_MUSIC"
+        })
+    }
+}
+
+export const pauseMusic = () => {
+    return async dispatch => {
+        dispatch({
+            type: "PAUSE_MUSIC"
+        })
+    }
+}
+
+export const increaseMusic = () => {
+    return async dispatch => {
+        dispatch({
+            type: "INCREASE_MUSIC"
+        })
+    }
+}
+
+export const decreaseMusic = () => {
+    return async dispatch => {
+        dispatch({
+            type: "DECREASE_MUSIC"
+        })
+    }
+}
+
+export const changeMusic = () => {
+    return async dispatch => {
+        dispatch({
+            type: "CHANGE_MUSIC"
+        })
+    }
+}
