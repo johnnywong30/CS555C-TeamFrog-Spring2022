8ee96f03cb47884830dc740af02ade928276ec9a
Sophia Zuo <sophiazuo@Sophias-MacBook-Pro-2.local>
2022-03-01 00:47:40 -0500

    user story 6 - starter frog backend

---
+++ server/routes/frog.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const frogs = require('../data/frogs')
+
+router
+    .route('/updateFrogName')
+    .post(async (req, res) => {
+        try {
+            const { name, newName } = req.body
+            const frog = await frogs.updateFrogName(name, newName)
+            res.json(frog).end()
+        } catch(e) {
+            console.log(e)
+            res.statusMessage = e
+            res.status(200).json({ errorMsg: e}).end()
+        }
+    })
+router
+    .route('/updateFrogLink')
+    .post(async (req, res) => {
+        try {
+            const { name, newLink } = req.body
+            const frog = await frogs.updateFrogLink(name, newLink)
+            res.json(frog).end()
+        } catch(e) {
+            console.log(e)
+            res.statusMessage = e
+            res.status(200).json({ errorMsg: e}).end()
+        }
+    })
+module.exports = router;
