92d078d454ddd67a75fa30f60a8d4a83a7d6cbe1
Jordan Wang <jordanwng58@gmail.com>
2022-03-16 17:41:25 -0400

    setup frontend for friends

---
+++ client/src/routes/Pages/Friends/AddFriend.js
@@ -0,0 +1,10 @@
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton} from '@chakra-ui/react'
+import * as React from 'react'
+import { useEffect, useState} from 'react'
+import { useDispatch } from 'react-redux'
+
+const AddFriend = () => {
+    // hook to use redux actions
+}
+
+export default AddFriend;
