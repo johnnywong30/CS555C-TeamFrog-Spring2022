1558f1f4d68d84529b007d5c48e224487e0cd802
Nikky5236 <57335776+Nikky5236@users.noreply.github.com>
2022-03-21 09:47:14 -0400

    error checking and money update

--- client/src/routes/Pages/Profile/index.js
+++ client/src/routes/Pages/Profile/index.js
@@ -1,31 +1,29 @@
-import React from 'react'
-import { Container} from '@chakra-ui/react'
+import React from "react";
+import { Container } from "@chakra-ui/react";
 
-
-import Layout from '../../../components/layouts/Layout'
-import Form from './Form'
+import Layout from "../../../components/layouts/Layout";
+import Form from "./Form";
 
 export const Profile = () => {
-    return (
-        <Layout>
-            <Container
-                maxW="lg"
-                py={{
-                    base: '12',
-                    md: '24',
-                }
-                }
-                px={{
-                    base: '0',
-                    sm: '8',
-                }}
-            >
-                <Form />
-               {/* // TODO Make editable profile form of relevant info  */}
-               {/* // TODO Make water history section  */}
-               {/* // TODO Make completed challenge history section  */}
-               {/* // TODO Make title section here or put with frogs*/}
-            </Container>
-        </Layout>
-    );
-}
+	return (
+		<Layout>
+			<Container
+				maxW="lg"
+				py={{
+					base: "12",
+					md: "24",
+				}}
+				px={{
+					base: "0",
+					sm: "8",
+				}}
+			>
+				<Form />
+				{/* // TODO Make editable profile form of relevant info  */}
+				{/* // TODO Make water history section  */}
+				{/* // TODO Make completed challenge history section  */}
+				{/* // TODO Make title section here or put with frogs*/}
+			</Container>
+		</Layout>
+	);
+};
--- server/misc/validate.js
+++ server/misc/validate.js
@@ -1,9 +1,14 @@
 module.exports = {
-    checkStr(str) {
-        if (! str) throw 'string does not exist'
-        if (typeof str !== 'string') throw 'input is not a string'
-        const trimmed = str.trim()
-        if (trimmed.length < 1) throw 'input cannot be empty string'
-        return trimmed
-    }
-}
+	checkStr(str) {
+		if (!str) throw "string does not exist";
+		if (typeof str !== "string") throw "input is not a string";
+		const trimmed = str.trim();
+		if (trimmed.length < 1) throw "input cannot be empty string";
+		return trimmed;
+	},
+	checkNum(num) {
+		if (!num) throw "number does not exist";
+		if (typeof int !== "number") throw "input is not a number";
+		return num;
+	},
+};
