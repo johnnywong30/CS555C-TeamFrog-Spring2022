76a7f1b4d7aeca26fb269ab8663485ecb68f5c49
Jordan Wang <jordanwng58@gmail.com>
2022-03-28 15:07:17 -0400

    setting up friend.js

--- client/src/routes/Pages/Friends/Friend.js
+++ client/src/routes/Pages/Friends/Friend.js
@@ -1,13 +1,25 @@
-import { Alert, AlertIcon, AlertTitle, AlertDescription, Box, Button, CloseButton, Checkbox, Container, Divider, FormControl, FormLabel, Heading, HStack, Input, Stack, Text, useBreakpointValue, useColorModeValue, VStack, useMergeRefs } from '@chakra-ui/react'
+import { Alert, AlertIcon, AlertTitle, AlertDescription, Box, Button, CloseButton, Checkbox, Container, Divider, FormControl, FormLabel, Heading, HStack, Grid, GridItem, Input, Stack, Text, useBreakpointValue, useColorModeValue, VStack, useMergeRefs } from '@chakra-ui/react'
 import * as React from 'react'
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Mongo from '../../../services/mongo';
 
-export const Friend = () = React.forwardRef((props, ref) => {
+export const Friend = React.forwardRef((props, ref) => {
     const { email } = props;
     const inputRef = React.useRef(null)
     const mergeRef = useMergeRefs(inputRef, ref)
 
+    // grid + divider
+    return (
+        <Grid templateColumns='repeat(5, 1fr)' gap={6}>
+            <GridItem w='100%' h='10'/>
+            <GridItem w='100%' h='10'/>
+            <GridItem w='100%' h='10'/>
+            <GridItem w='100%' h='10'/>
+            <GridItem w='100%' h='10'/>
+        </Grid>
+    )
 })
+
+export default Friend;
