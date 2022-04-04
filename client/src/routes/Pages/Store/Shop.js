a25da53a881e75b4d0eb7a2020bd64289869af38
Nikky5236 <57335776+Nikky5236@users.noreply.github.com>
2022-04-04 17:35:58 -0400

    complete
    implemented us 24 store progress and us 28 view frog store. No mongodb functions added so no additional tests made. Made room for spending currency via onClick operations in Store

--- client/src/routes/Pages/Store/Shop.js
+++ client/src/routes/Pages/Store/Shop.js
@@ -1,23 +1,45 @@
-import React from "react";
-// import { Link } from "react-router-dom";
-// import PropTypes from "prop-types";
-import { Box, Flex, HStack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { SimpleGrid, Box, Flex, HStack } from "@chakra-ui/react";
 import { Listing } from "./froglisting";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { AiFillRightCircle } from "react-icons/ai";
 
 const Shop = () => {
+	const { ownedFrogs } = useSelector(({ auth }) => auth.user);
+	const frogs = [];
+	const [repo, setRepo] = useState([]);
+
+	const getRepo = () => {
+		axios.get(`/frog/getFrogs`).then(response => {
+			const myRepo = response.data;
+			setRepo(myRepo);
+		});
+	};
+
+	useEffect(() => getRepo(), []);
+
+	let own = true;
+	for (let i = 0; i < repo.length; i++) {
+		const frogID = ownedFrogs[i];
+		if (frogID === undefined) own = false;
+		frogs.push({ url: repo[i].url, name: repo[i].name, owns: own });
+	}
+
 	return (
-		<Flex align="center" justify={{ base: "center", md: "space-around", xl: "space-between" }} direction={{ base: "column-reverse", md: "row" }} wrap="no-wrap" minH="70vh" px={8} mb={16}>
-			<HStack spacing="24px">
-				<Box border="4px" borderColor="white" w={{ base: "80%", sm: "60%", md: "50%" }} h={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }} rounded="1rem" shadow="2xl" border-color="white">
-					<Listing frogLink="https://i.imgur.com/VJeksGH.png" frogName="Random Frog 1" price="500"></Listing>
-				</Box>
-				<Box border="4px" borderColor="white" w={{ base: "80%", sm: "60%", md: "50%" }} h={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }} rounded="1rem" shadow="2xl" border-color="white">
-					<Listing frogLink="https://i.imgur.com/VJeksGH.png" frogName="Random Frog 2" price="1000"></Listing>
-				</Box>
-				<Box border="4px" borderColor="white" w={{ base: "80%", sm: "60%", md: "50%" }} h={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }} rounded="1rem" shadow="2xl" border-color="white">
-					<Listing frogLink="https://i.imgur.com/VJeksGH.png" frogName="Random Frog 3" price="2000"></Listing>
-				</Box>
-			</HStack>
+		<Flex
+			align="center"
+			justify={{ base: "center", md: "space-around", xl: "space-between" }}
+			direction={{ base: "column-reverse", md: "row" }}
+			wrap="no-wrap"
+			minH="70vh"
+			px={8}
+			mb={16}>
+			<SimpleGrid columns={3} spacingX="10px" spacingY="10px">
+				{frogs.map(frog => (
+					<Listing frogLink={frog.url} frogName={frog.name} own={frog.owns} price="500"></Listing>
+				))}
+			</SimpleGrid>
 		</Flex>
 	);
 };
--- server/data/frogs.js
+++ server/data/frogs.js
@@ -1,112 +1,111 @@
-const { frogs } = require('../config/mongoCollections')
-const { ObjectId } = require('mongodb')
-const { checkStr, checkNum } = require('../misc/validate')
+const { frogs } = require("../config/mongoCollections");
+const { ObjectId } = require("mongodb");
+const { checkStr, checkNum } = require("../misc/validate");
 
 module.exports = {
-    async getFrog(_name) {
-        const name = checkStr(_name)
-        const collection = await frogs()
-        const frog = await collection.findOne({ name: name })
-        if (frog === null) return []
-        return [frog]
-    },
+	async getFrog(_name) {
+		const name = checkStr(_name);
+		const collection = await frogs();
+		const frog = await collection.findOne({ name: name });
+		if (frog === null) return [];
+		return [frog];
+	},
 
-    async getFrogLink(_id) {
-        const id = checkStr(_id)
-        const collection = await frogs()
-        const frog = await collection.findOne({frogId: id})
-        if (frog === null) return []
-        return frog.url
-    },
+	async getFrogLink(_id) {
+		const id = checkStr(_id);
+		const collection = await frogs();
+		const frog = await collection.findOne({ frogId: id });
+		if (frog === null) return [];
+		return frog.url;
+	},
 
-    async getFrogs() {
-        const collection = await frogs()
-        const frogList = await collection.find({}).toArray()
-        if (!frogList) throw 'could not get all users'
-        return frogList
-    },
+	async getFrogs() {
+		const collection = await frogs();
+		const frogList = await collection.find({}).project({}).toArray();
+		if (!frogList) throw "could not get all users";
+		return frogList;
+	},
 
-    async getFrogUrls() {
-        const collection = await frogs()
-        const frogList = await collection.find({}).project({ frogId: 1, url: 1, _id: 0 }).toArray()
-        console.log(frogList)
-        if (!frogList) throw 'could not get all urls'
-        return frogList
-    },
+	async getFrogUrls() {
+		const collection = await frogs();
+		const frogList = await collection.find({}).project({ frogId: 1, url: 1, _id: 0 }).toArray();
+		if (!frogList) throw "could not get all urls";
+		return frogList;
+	},
 
-    async createFrog(_frogId, _name, _url) {
-        const frogId = checkNum(_frogId);
-        const name = checkStr(_name);
-        const url = checkStr(_url)
-        const frogExists = await this.getFrog(name)
-        if (frogExists.length > 0) throw 'Frog with this name exists'
-        const newFrog = {
-            frogId: frogId,
-            name: name,
-            url: url
-        }
-        const collection = await frogs();
-        const insertInfo = await collection.insertOne(newFrog)
-        if (! insertInfo.acknowledged || ! insertInfo.insertedId) throw 'Could not register frog'
-        const frog = await this.getFrog(name)
-        if (frog.length < 1) throw 'Could not get frog'
-        const ret = {
-            ...frog[0]
-        }
-        return ret
-    },
+	async createFrog(_frogId, _name, _url) {
+		const frogId = checkNum(_frogId);
+		const name = checkStr(_name);
+		const url = checkStr(_url);
+		const frogExists = await this.getFrog(name);
+		if (frogExists.length > 0) throw "Frog with this name exists";
+		const newFrog = {
+			frogId: frogId,
+			name: name,
+			url: url,
+		};
+		const collection = await frogs();
+		const insertInfo = await collection.insertOne(newFrog);
+		if (!insertInfo.acknowledged || !insertInfo.insertedId) throw "Could not register frog";
+		const frog = await this.getFrog(name);
+		if (frog.length < 1) throw "Could not get frog";
+		const ret = {
+			...frog[0],
+		};
+		return ret;
+	},
 
-    async deleteFrog(_name) {
-        const name = checkStr(_name)
-        const collection = await frogs()
-        const deletionInfo = await collection.deleteOne({name: name})
-        if (deletionInfo.deleteCount === 0) throw `Could not delete frog ${name}`
-        return true
-    },
-    
-    async updateFrogName(_name, _newName) {
-        const name = checkStr(_name);
-        const newName = checkStr(_newName);
-        const frogExists = await this.getFrog(name)
-        if (frogExists.length < 1) throw 'This frog does not exist'
-        const frog = frogExists[0]
-        if (frog.name === newName) throw 'New frog name cannot be the same as the original'
-        const collection = await frogs()
-        const updatedFrog = {
-            ...frog,
-            name: newName
-        }
-        const updateInfo = await collection.updateOne({name: name}, {$set: updatedFrog})
-        if (updateInfo.modifiedCount === 0) throw `Could not update frog successfully`
-        const updated = await this.getFrog(newName)
-        if (updated.length < 1) throw 'Could not get frog'
-        const data = updated[0]
-        return {
-            ...data,
-            successMsg: 'Successfully updated frog name'
-        }
-    },
+	async deleteFrog(_name) {
+		const name = checkStr(_name);
+		const collection = await frogs();
+		const deletionInfo = await collection.deleteOne({ name: name });
+		if (deletionInfo.deleteCount === 0) throw `Could not delete frog ${name}`;
+		return true;
+	},
 
-    async updateFrogLink(_name, _newLink) {
-        const name = checkStr(_name)
-        const link = checkStr(_newLink)
-        const frogExists = await this.getFrog(name)
-        if (frogExists.length < 1) throw 'This frog does not exist'
-        const frog = frogExists[0]
-        if (frog.url === link) throw 'New frog name cannot be the same as the original'
-        const collection = await frogs()
-        const updatedFrog = {
-            ...frog,
-            url: link
-        }
-        const updateInfo = await collection.updateOne({name: name}, {$set: updatedFrog})
-        if (updateInfo.modifiedCount < 1) throw `Could not update frog successfully`
-        const updated = await this.getFrog(name)
-        if (updated.length < 1) throw 'Could not get frog'
-        const data = updated[0]
-        return {
-            ...data,
-            successMsg: 'Successfully updated frog link'
-        }
-    }
-}
+	async updateFrogName(_name, _newName) {
+		const name = checkStr(_name);
+		const newName = checkStr(_newName);
+		const frogExists = await this.getFrog(name);
+		if (frogExists.length < 1) throw "This frog does not exist";
+		const frog = frogExists[0];
+		if (frog.name === newName) throw "New frog name cannot be the same as the original";
+		const collection = await frogs();
+		const updatedFrog = {
+			...frog,
+			name: newName,
+		};
+		const updateInfo = await collection.updateOne({ name: name }, { $set: updatedFrog });
+		if (updateInfo.modifiedCount === 0) throw `Could not update frog successfully`;
+		const updated = await this.getFrog(newName);
+		if (updated.length < 1) throw "Could not get frog";
+		const data = updated[0];
+		return {
+			...data,
+			successMsg: "Successfully updated frog name",
+		};
+	},
+
+	async updateFrogLink(_name, _newLink) {
+		const name = checkStr(_name);
+		const link = checkStr(_newLink);
+		const frogExists = await this.getFrog(name);
+		if (frogExists.length < 1) throw "This frog does not exist";
+		const frog = frogExists[0];
+		if (frog.url === link) throw "New frog name cannot be the same as the original";
+		const collection = await frogs();
+		const updatedFrog = {
+			...frog,
+			url: link,
+		};
+		const updateInfo = await collection.updateOne({ name: name }, { $set: updatedFrog });
+		if (updateInfo.modifiedCount < 1) throw `Could not update frog successfully`;
+		const updated = await this.getFrog(name);
+		if (updated.length < 1) throw "Could not get frog";
+		const data = updated[0];
+		return {
+			...data,
+			successMsg: "Successfully updated frog link",
+		};
+	},
+};
