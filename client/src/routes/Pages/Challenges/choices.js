de689399444c2a3e7440d2e8aefbf26ee16dad59
Eric Stazzone <estazzon@stevens.edu>
2022-04-04 21:22:48 -0400

    Accepted Verification

--- client/src/routes/Pages/Challenges/choices.js
+++ client/src/routes/Pages/Challenges/choices.js
@@ -10,38 +10,58 @@ import Mongo from '../../../services/mongo';
 
 const AllChallenges = () => {
     const dispatch = useDispatch()
-    const [accepted, setAccept] = useSelector({disabled: false, text: "Accept"})
-    const { challenges } = useSelector(({ auth }) => auth.user)
+    const { email, challenges } = useSelector(({ auth }) => auth.user) //completedChallenges
+
+    const challengesArr = [
+        {name: 'Challenge 1', description: 'Drink 10 cups of water in a week'},
+        {name: 'Challenge 2', description: 'Drink 10 cups of water in a week'},
+        {name: 'Challenge 3', description: 'Drink 10 cups of water in a week'},
+        {name: 'Challenge 4', description: 'Drink 10 cups of water in a week'},
+        {name: 'Challenge 5', description: 'Drink 10 cups of water in a week'},
+    ]
+
+    const mappedChallenges = challengesArr.map(challenge => {
+        return {
+            name: challenge.name,
+            description: challenge.description,
+            accepted: challenges.includes(challenge.name),
+            //completed: completedChallenges.includes(challenge.name)
+        }
+    })
 
     const accept = (e) => {
         e.preventDefault();
         dispatch(Mongo.updateChallenges(email, e.target.value));
-		if (accepted.disabled === false) {
-			setAccept({disabled: true, text: "Accepted" });
-		}
 	};
 
     return (
         <Accordion allowMultiple>
-            <AccordionItem>
-                <h2>
-                <AccordionButton>
-                    <Box flex='1' textAlign='left'>
-                        <Stack align='center' direction='center'>
-                            <Box flex='1' textAlign='left' fontWeight='bold' fontSize = 'md'>Challenge 1</Box>
-                        </Stack>
-                    </Box>
-                    <AccordionIcon />
-                </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                <Box fontSize = 'sm'>Description</Box>
-                <Stack align='center' direction='center' marginTop = '1'>
-                    <Button onClick={accept} disabled={accepted.disabled} colorScheme='green' size='sm' marginRight = '1' value={'challenge1'}>{accepted.text}</Button>
-                    <Button colorScheme='green' size='sm' variant='outline'>Complete</Button>
-                </Stack>
-                </AccordionPanel>
-            </AccordionItem>
+            {mappedChallenges.map(challenge => {
+                const name = challenge.name
+                const description = challenge.description
+                const accepted = challenge.accepted
+                return (
+                    <AccordionItem>
+                        <h2>
+                        <AccordionButton>
+                            <Box flex='1' textAlign='left'>
+                                <Stack align='center' direction='center'>
+                                    <Box flex='1' textAlign='left' fontWeight='bold' fontSize = 'md'>{name}</Box>
+                                </Stack>
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                            <Box fontSize = 'sm'>{description}</Box>
+                            <Stack align='center' direction='center' marginTop = '1'>
+                                <Button isDisabled = {accepted} onClick={accept} colorScheme='green' size='sm' marginRight = '1' value={name}>{accepted ? "Accepted" : "Accept"}</Button>
+                                <Button colorScheme='green' size='sm' variant='outline'>Complete</Button>
+                            </Stack>
+                        </AccordionPanel>
+                    </AccordionItem>
+                )
+            })}
         </Accordion>  
     )
 }
