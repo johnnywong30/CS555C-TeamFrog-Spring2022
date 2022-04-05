3d455819727be000e5e76174780dfd646dca8b1c
Johnny Wong <johnny.wong2324@gmail.com>
2022-04-05 19:13:47 -0400

    title routes added

--- server/routes/user.js
+++ server/routes/user.js
@@ -121,11 +121,38 @@ router
             res.json(user).end()
         } catch (e) {
             console.log(e)
-            console.log(e)
             res.statusMessage = e
             res.status(200).json({ errorMsg: e }).end()
         }
     }) 
+
+router
+    .route('/addTitle')
+    .post(async (req, res) => {
+        try {
+            const { email, title } = req.body
+            const user = await users.addTitle(email, title)
+            res.json(user).end()
+        } catch (e) {
+            console.log(e)
+            res.statusMessage = e
+            res.status(200).json({ errorMsg: e }).end()
+        }
+    })
+
+router
+    .route('/updateTitle')
+    .post(async (req, res) => {
+        try {
+            const { email, titleIndex } = req.body
+            const user = await users.updateTitle(email, titleIndex)
+            res.json(user).end()
+        } catch (e) {
+            console.log(e)
+            res.statusMessage = e
+            res.status(200).json({ errorMsg: e }).end()
+        }
+    })
 // TODO: the rest of the update routes
 
 module.exports = router;
