29f3bb1ee8f9e0984a07004da41747d962e5ddd0
Johnny Wong <johnny.wong2324@gmail.com>
2022-04-05 21:44:44 -0400

    documented frog title data

--- client/src/constants/titles.js
+++ client/src/constants/titles.js
@@ -1,10 +1,10 @@
 const titleData = {
-    start: 'Tadpole',
-    beluga: 'Beluga Frog',
-    hydrated: 'Hydrated Frog',
-    purple: 'Scrum Frog',
-    orange: 'Tyler Terry',
-    perfect: 'Perfectionist',
+    start: 'Tadpole', // Start with this
+    beluga: 'Beluga Frog', // If you buy any frog
+    hydrated: 'Hydrated Frog', // If you hydrate
+    purple: 'Scrum Frog', // If you buy purple frog
+    orange: 'Tyler Terry', // If you buy orange frog
+    perfect: 'Perfectionist', // If you buy all of the frogs in store
 }
 
 export default titleData;
