e26a222f3dabc4c6910c6465cc5943bd5a4407bf
Sophia Zuo <sophiasaur.zuo@gmail.com>
2022-04-18 12:35:52 -0400

    frog subtitle

--- client/src/routes/Pages/Collection/index.js
+++ client/src/routes/Pages/Collection/index.js
@@ -10,39 +10,40 @@ import { GiFrogFoot } from 'react-icons/gi'
 import audio from '../../../../src/constants/frog.wav';
 import Titles from './Titles';
 import { FrogName } from './FrogName'
+import FrogBox from './FrogBox';
 
 export const Collection = () => {
-    const dispatch = useDispatch()
+    // const dispatch = useDispatch()
     const { ownedFrogs, email, frog, frogNames } = useSelector(({ auth }) => auth.user)
     const { store } = useSelector(({ auth }) => auth)
-    const [friendFrogs, setFriendFrogs] = useState([])
-    const [play] = useSound(audio);
+    // const [friendFrogs, setFriendFrogs] = useState([])
+    // const [play] = useSound(audio);
 
     const location = useLocation();
     const friendEmail = location?.state?.friendEmail
 
     const frogEmail = friendEmail !== undefined ? friendEmail : email
-    const mysteryFrogUrl = 'https://imgur.com/VJeksGH.png'
+    // const mysteryFrogUrl = 'https://imgur.com/VJeksGH.png'
 
-    const getFriendFrogs = async () => {
-        axios.get(`/user/getOwnedFrogs/${frogEmail}`).then(response => {
-            const friendOwnedFrogs = response.data;
-            setFriendFrogs(friendOwnedFrogs)
-        });
-    };
+    // const getFriendFrogs = async () => {
+    //     axios.get(`/user/getOwnedFrogs/${frogEmail}`).then(response => {
+    //         const friendOwnedFrogs = response.data;
+    //         setFriendFrogs(friendOwnedFrogs)
+    //     });
+    // };
 
-    const getFrogs = async () => {
-        dispatch(Mongo.getFrogList())
-    }
+    // const getFrogs = async () => {
+    //     dispatch(Mongo.getFrogList())
+    // }
 
-    useEffect(async () => {
-        await getFrogs()
-        if (friendEmail !== undefined) await getFriendFrogs()
-    }, []);
+    // useEffect(async () => {
+    //     await getFrogs()
+    //     if (friendEmail !== undefined) await getFriendFrogs()
+    // }, []);
 
-    const selectFrog = (e) => {
-        dispatch(Mongo.updateFrog(email, e.target.value))
-    }
+    // const selectFrog = (e) => {
+    //     dispatch(Mongo.updateFrog(email, e.target.value))
+    // }
 
     return (
         <Layout>
@@ -65,27 +66,22 @@ export const Collection = () => {
                 <SimpleGrid columns={3} spacing={10}>
                     {store.map(element => {
                         const { _id, frogId, url, name } = element
-                        const owned = friendEmail !== undefined ? friendFrogs.includes(frogId) : ownedFrogs.includes(frogId)
-                        const onClick = owned ? play : console.log('Mystery ribbit')
-                        const imgUrl = owned ? url : mysteryFrogUrl
-                        const selected = frogId === frog
-                        const buttonText = selected ? 'Selected' : 'Select'
-                        // Get the name of the user's frog so we can update the button text
-                        const checkName = frogNames.find(obj => obj.id === frogId)
-                        const frogName = (checkName !== undefined && owned) ? checkName.name : ""
+                        // const owned = friendEmail !== undefined ? friendFrogs.includes(frogId) : ownedFrogs.includes(frogId)
+                        // const onClick = owned ? play : console.log('Mystery ribbit')
+                        // const imgUrl = owned ? url : mysteryFrogUrl
+                        // const selected = frogId === frog
+                        // const buttonText = selected ? 'Selected' : 'Select'
+                        // // Get the name of the user's frog so we can update the button text
+                        // const checkName = frogNames.find(obj => obj.id === frogId)
+                        // const frogName = (checkName !== undefined && owned) ? checkName.name : ""
                         // currently does not display friends' custom names for frogs and only displays user's
                         return (
-                            <Box key={_id} height='275px' mb={'1rem'}>
-                                <FrogName frogId={frogId} frogName={frogName} defaultName={""} isDisabled={!owned} friendEmail={friendEmail !== undefined}></FrogName>
-                                <Image onClick={onClick} src={imgUrl} rounded="1rem" shadow="2xl" />
-                                {(friendEmail === undefined && owned) &&
-                                    <Center>
-                                        <Button value={frogId} onClick={selectFrog} size='sm' variant='unstyled' colorScheme='blackAlpha' isDisabled={selected} leftIcon={<GiFrogFoot />}>
-                                            {buttonText}
-                                        </Button>
-                                    </Center>
-                                }
-                            </Box>
+                            <FrogBox
+                                _id={_id}
+                                frogId={frogId}
+                                url={url}
+                                name={name}
+                            />
                         )
                     })}
                 </SimpleGrid>
